7e1ff47991edfa4b02bc12a18de3497b45083f5e
Heorhii Savostikov <georgesavostikov@gmail.com>
2026-03-29 14:30:43 +0300

    Added some new tests for the function

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -3,6 +3,8 @@
 const { reduce } = require('./reduce');
 
 describe('reduce', () => {
+  let array = [1, 2, 3, 4, 5, 6];
+
   beforeAll(() => {
     Array.prototype.reduce2 = reduce; // eslint-disable-line
   });
@@ -11,7 +13,69 @@ describe('reduce', () => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {});
+  it('should be declared', () => {
+    expect(Array.prototype.reduce2).toBeInstanceOf(Function);
+  });
 
-  // Add tests here
+  it('should sum array with initial value', () => {
+    const callback = jest.fn((prev, x) => prev + x);
+
+    const result = array.reduce2(callback, 0);
+
+    expect(result).toBe(21);
+    expect(callback).toHaveBeenCalledTimes(6);
+  });
+
+  it('should sum array without initial value', () => {
+    const callback = jest.fn((prev, x) => prev + x);
+
+    const result = array.reduce2(callback);
+
+    expect(result).toBe(21);
+    expect(callback).toHaveBeenCalledTimes(5);
+  });
+
+  it('should return initial value for empty array', () => {
+    array = [];
+
+    const callback = jest.fn((prev, x) => prev + x);
+
+    const result = array.reduce2(callback, 0);
+
+    expect(result).toBe(0);
+    expect(callback).not.toHaveBeenCalled();
+  });
+
+  it('should return undefined for empty array without initial value', () => {
+    array = [];
+
+    const callback = jest.fn((prev, x) => prev + x);
+
+    const result = array.reduce2(callback);
+
+    expect(result).toBe(undefined);
+    expect(callback).not.toHaveBeenCalled();
+  });
+
+  it('should work with single element and initial value', () => {
+    array = [1];
+
+    const callback = jest.fn((prev, x) => prev + x);
+
+    const result = array.reduce2(callback, 2);
+
+    expect(result).toBe(3);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should return element if single element and no initial value', () => {
+    array = [1];
+
+    const callback = jest.fn((prev, x) => prev + x);
+
+    const result = array.reduce2(callback);
+
+    expect(result).toBe(1);
+    expect(callback).not.toHaveBeenCalled();
+  });
 });
